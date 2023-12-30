8c8c56b1ea3b90986e4d6f188f2429bc94c3c19a
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-12-30 23:07:24 +0100

    Update files

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
+
 app.listen(port, () => {
     console.log('Server is listening on port: ' + port)
 });
