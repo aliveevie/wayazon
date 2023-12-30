7dff867d349bcee5b88e66daa4f68c55f6f36c14
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-12-30 17:42:55 +0100

    Update files

--- frontend/src/data/teams.js
+++ frontend/src/data/teams.js
@@ -1,54 +1,63 @@
-const data = [
-    {
-      name: 'Shamsuddeen MUHAMMAD',
-      title: 'Secretary',
-      image: '../images/teams/shamsi.png',
-      description: `
-        Shamsuddeen Muhammed, a Botany graduate from Federal University Dutse, Jigawa State, with a dedication to excellence. Ready to serve customers with efficiency and meet deadlines effectively. 
-        My academic background and commitment position me to contribute diligently in any professional setting.
-      `
-    },
-    {
-      name: 'Dahiru Aliyu Musa',
-      title: 'Business IT and Customer Care Manager',
-      image: '../images/teams/dahiru.png',
-      description: `
-        Dahiru Aliyu, a Computer Science graduate from Sule Lamido University, Kafin Hausa, and a proficient computer guru. Known for swift resolution of internet-related issues, I bring technical expertise and a proactive approach to problem-solving. Ready to contribute my skills for seamless digital experiences.
-      `
-    },
-    {
-      name: 'Nura M Usman',
-      title: 'Logistics and Delivery Manager',
-      image: '../images/teams/nura.png',
-      description: `
-        Usman Ado, a Business Administration and Management Studies graduate from Jigawa State Polytechnic, Dutse. Committed to serving customers promptly within specified timeframes, I prioritize honesty and trustworthiness in all endeavors. Eager to contribute my skills and dedication to achieve mutual success in any business engagements.
-      `
-    },
-    {
-      name: 'Usman Ado',
-      title: 'Human Resources Manager',
-      image: '../images/teams/usmanado.png',
-      description: `
-        Usman Ado, a Business Administration and Management Studies graduate from Jigawa State Polytechnic, Dutse. Committed to serving customers promptly within specified timeframes, I prioritize honesty and trustworthiness in all endeavors. Eager to contribute my skills and dedication to achieve mutual success in any business engagements.
-      `
-    },
-    {
-      name: 'Safiyanu Ubale',
-      title: 'CEO',
-      image: '../images/teams/safiyyanu.png',
-      description: `
-        Safiyanu Ubale, an Economics graduate from Federal University Dutse, Jigawa State, showcasing a commitment to business excellence. Known for diligence and a strong work ethic, I am poised to apply my knowledge and skills in contributing meaningfully to professional and business endeavors toward serving our customers with the utmost sense of concern.
-      `
-    },
-    {
-        name: 'Abdul Rahman Tijjani',
-        title: 'Marketing Manager',
-        image: '<Image Path>',
-        description: `
-          Abdul Rahman Tijjani, a dynamic Building Technologist graduated from Jigawa State Polytechnic, Dutse, and a business prodigy driven by a fervent passion for entrepreneurship. Bringing a unique blend of technical expertise and business acumen, I am poised to excel in business endeavors, leveraging my skills to contribute to innovative and successful ventures.
-        `
-      }
-  ];
+import React from 'react';
 
-  export default data;
-  
+// Import images
+import shamsiImage from '../images/teams/shamsi.png';
+import dahiruImage from '../images/teams/dahiru.png';
+import nuraImage from '../images/teams/nura.png';
+import usmanAdoImage from '../images/teams/usmanado.png';
+import safiyyanuImage from '../images/teams/safiyyanu.png';
+import abdul from '../images/teams/abdul.png';
+
+const data = [
+  {
+    name: 'Shamsuddeen MUHAMMAD',
+    title: 'Secretary',
+    image: shamsiImage,
+    description: `
+      Shamsuddeen Muhammed, a Botany graduate from Federal University Dutse, Jigawa State, with a dedication to excellence. Ready to serve customers with efficiency and meet deadlines effectively. 
+      My academic background and commitment position me to contribute diligently in any professional setting.
+    `
+  },
+  {
+    name: 'Dahiru Aliyu Musa',
+    title: 'Business IT and Customer Care Manager',
+    image: dahiruImage,
+    description: `
+      Dahiru Aliyu, a Computer Science graduate from Sule Lamido University, Kafin Hausa, and a proficient computer guru. Known for swift resolution of internet-related issues, I bring technical expertise and a proactive approach to problem-solving. Ready to contribute my skills for seamless digital experiences.
+    `
+  },
+  {
+    name: 'Nura M Usman',
+    title: 'Logistics and Delivery Manager',
+    image: nuraImage,
+    description: `
+      Usman Ado, a Business Administration and Management Studies graduate from Jigawa State Polytechnic, Dutse. Committed to serving customers promptly within specified timeframes, I prioritize honesty and trustworthiness in all endeavors. Eager to contribute my skills and dedication to achieve mutual success in any business engagements.
+    `
+  },
+  {
+    name: 'Usman Ado',
+    title: 'Human Resources Manager',
+    image: usmanAdoImage,
+    description: `
+      Usman Ado, a Business Administration and Management Studies graduate from Jigawa State Polytechnic, Dutse. Committed to serving customers promptly within specified timeframes, I prioritize honesty and trustworthiness in all endeavors. Eager to contribute my skills and dedication to achieve mutual success in any business engagements.
+    `
+  },
+  {
+    name: 'Safiyanu Ubale',
+    title: 'CEO',
+    image: safiyyanuImage,
+    description: `
+      Safiyanu Ubale, an Economics graduate from Federal University Dutse, Jigawa State, showcasing a commitment to business excellence. Known for diligence and a strong work ethic, I am poised to apply my knowledge and skills in contributing meaningfully to professional and business endeavors toward serving our customers with the utmost sense of concern.
+    `
+  },
+  {
+    name: 'Abdul Rahman Tijjani',
+    title: 'Marketing Manager',
+    image: abdul,
+    description: `
+      Abdul Rahman Tijjani, a dynamic Building Technologist graduated from Jigawa State Polytechnic, Dutse, and a business prodigy driven by a fervent passion for entrepreneurship. Bringing a unique blend of technical expertise and business acumen, I am poised to excel in business endeavors, leveraging my skills to contribute to innovative and successful ventures.
+    `
+  }
+];
+
+export default data;
--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -1,8 +1,9 @@
 import data from '../data/teams'
+import '../styles/about.css'
+import dahiru from '../images/teams/dahiru.png'
 
 export function About(){
 
-    console.log(data)
     return (
         <div className="about-us" >
             <h2>Wayazone</h2>
@@ -13,6 +14,25 @@ export function About(){
             Dependability and efficiency are given top priority by Wayazone, which is dedicated to meeting customer needs. For both buyers and sellers, our platform guarantees a quick, easy, and transparent transaction procedure while facilitating safe transactions. Our goal is to use technology to develop a dynamic online marketplace that meets changing consumer demands.
 
             By embracing innovation, Wayazone hopes to establish itself as a reliable resource for tech aficionados and build a community that values state-of-the-art gadgets and accessories. Wayazone, a local company with a worldwide outlook, is poised to improve e-commerce landscape by lowering the cost of high-quality electronic products for everyone.</p>
+        
+            <div className="our-team">
+       
+        <div className="team-members">
+        <h3>Meet Our Team</h3>
+          {data.map((member) => (
+            <div key={member.name} className="team-member">
+              <div className="member-image">
+                <img src={member.image} alt={member.name} />
+              </div>
+              <div className="member-info">
+                <h3>{member.name}</h3>
+                <p>{member.title}</p>
+                <p>{member.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
         </div>
     )
 }
