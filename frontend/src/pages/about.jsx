ee8d2bb6ba01914c3b5de6a9ebc60e82a7660ee6
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-12-31 10:27:34 +0100

    Remove node_modules

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -1,24 +1,22 @@
 import data from '../data/teams'
 import '../styles/about.css'
-import dahiru from '../images/teams/dahiru.png'
+
 
 export function About(){
 
     return (
         <div className="about-us"  >
-            <h2 data-aos="fade-up" >Wayazone</h2>
-            <p data-aos="fade-right" >Wayazone: Jigawa State's E-Commerce
+            <h2 data-aos="zoom-in" >Wayazone Gadgets</h2>
+            <p data-aos="zoom-out" >With its headquarters located in Jigawa State, Wayazon Gadgets is a trailblazing e-commerce firm that specializes in the easy purchase and sale of phones and accessories. Our endeavor, which is centered on giving tech aficionados a convenient platform, seeks to revolutionize the local market by presenting a wide selection of high-quality products.
 
-            With its headquarters located in Jigawa State, Wayazone is a trailblazing e-commerce firm that specializes in the easy purchase and sale of phones and accessories. Our endeavor, which is centered on giving tech aficionados a convenient platform, seeks to revolutionize the local market by presenting a wide selection of high-quality products.
+Dependability and efficiency are given top priority by Wayazon Gadgets, which is dedicated to meeting customer needs. For both buyers and sellers, our platform guarantees a quick, easy, and transparent transaction procedure while facilitating safe transactions. Our goal is to use technology to develop a dynamic online marketplace that meets changing consumer demands.
 
-            Dependability and efficiency are given top priority by Wayazone, which is dedicated to meeting customer needs. For both buyers and sellers, our platform guarantees a quick, easy, and transparent transaction procedure while facilitating safe transactions. Our goal is to use technology to develop a dynamic online marketplace that meets changing consumer demands.
-
-            By embracing innovation, Wayazone hopes to establish itself as a reliable resource for tech aficionados and build a community that values state-of-the-art gadgets and accessories. Wayazone, a local company with a worldwide outlook, is poised to improve e-commerce landscape by lowering the cost of high-quality electronic products for everyone.</p>
+By embracing innovation, Wayazon Gadgets hopes to establish itself as a reliable resource for tech aficionados and build a community that values state-of-the-art gadgets and accessories. Wayazon Gadgets, a local company with a worldwide outlook, is poised to improve e-commerce landscape by lowering the cost of high-quality electronic products for everyone.</p>
         
             <div className="our-team">
        
         <div className="team-members">
-        <h4>Meet Our Team</h4>
+        <h4  data-aos="zoom-out" >Meet Our Team</h4>
           {data.map((member) => (
             <div key={member.name} className="team-member">
               <div className="member-image">
