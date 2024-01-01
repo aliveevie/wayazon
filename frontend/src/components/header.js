16b742f39a95d6c216b325e5e1822e96b1702033
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2024-01-01 21:10:22 +0100

    Update files

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,9 +1,28 @@
-import React from 'react';
+// Import necessary libraries and components
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/header.css';
 import logo from '../images/logo.png';
 
 export function Header({ mobile, handleMobile }) {
+  const [showCategories, setShowCategories] = useState(false);
+
+  const handleHover = () => {
+    setShowCategories(true);
+  };
+
+  const handleLeave = () => {
+    setShowCategories(false);
+  };
+
+  /*
+    London Used
+    Smart Android
+    Samsung
+    Iphone
+    Others
+  */
+
   return (
     <div className='header' id="header-menu">
       <div className="logo">
@@ -12,7 +31,44 @@ export function Header({ mobile, handleMobile }) {
       <nav className="navBar" id='desktop-menu'>
         <Link to="/about">About Us</Link>
         <Link to="/contact">Contact Us</Link>
-        <Link to="/categories">Categories</Link>
+        <Link to="/categories" onMouseEnter={handleHover} onMouseLeave={handleLeave}>
+          Categories
+          {showCategories && (
+            <div className='dropdown'>
+              <Link to="/categories/phones">Phones
+              <Link to="/categories/phones#londonused" >
+                London Used
+              </Link>
+              <Link to="/categories/phones#smartandroid" >
+                Smart Android
+              </Link>
+              <Link to="/categories/phones#samsung" >
+              Samsung  
+              </Link>
+              <Link to="/categories/phones#iphone" >
+              Iphone  
+              </Link>
+              <Link to="/categories/phones#others" >
+              Others  
+              </Link>
+          </Link>
+          <Link to="/categories/accessories">Accessories
+              <Link to="/categories/accessories#mp3andspeakers" >
+              MP3 And Speakers
+              </Link>
+              <Link to="/categories/accessories#bluetooth" >
+              Bluetooth and AirPhones      
+              </Link>
+              <Link to="/categories/accessories#smartwatch" >
+              Smart Watch 
+              </Link>
+              <Link to="/categories/accessories#screenguard" >
+              Screen Guard and Cover
+              </Link>
+          </Link>
+            </div>
+          )}
+        </Link>
         <Link to="/">Home</Link>
       </nav>
       <div id="hamburger" onClick={handleMobile}>
